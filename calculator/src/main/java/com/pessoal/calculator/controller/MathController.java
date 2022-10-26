32f2383606a66906b3503d3bfec1bf3b80d9cfff
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2022-10-26 16:26:23 -0300

    Resto das operacoes

--- calculator/src/main/java/com/pessoal/calculator/controller/MathController.java
+++ calculator/src/main/java/com/pessoal/calculator/controller/MathController.java
@@ -11,10 +11,53 @@ public class MathController {
 
     @GetMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        validateInput(numberOne, numberTwo);
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        validateInput(numberOne, numberTwo);
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        validateInput(numberOne, numberTwo);
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        validateInput(numberOne, numberTwo);
+        if(numberTwo.equals("0")) {
+            throw new UnsuportedMathOperationException("Division by zero is not allowed");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        validateInput(numberOne, numberTwo);
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @GetMapping("/square-root/{number}")
+    public Double squareRoot(@PathVariable("number") String number) {
+        validateInput(number);
+        return Math.sqrt(convertToDouble(number));
+    }
+
+    private void validateInput(String number){
+        if(!isNumeric(number)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value");
+        }
+    }
+
+    private void validateInput(String numberOne, String numberTwo) {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsuportedMathOperationException("Please set a numeric value!");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
     private boolean isNumeric(String strNumber) {
