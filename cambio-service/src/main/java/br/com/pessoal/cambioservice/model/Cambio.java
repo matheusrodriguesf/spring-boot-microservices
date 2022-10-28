1e73f6a3e0606b5827bbd2efc99e0b3630d903bb
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2022-10-28 15:33:23 -0300

    Projeto Base Cambio Service

---
+++ cambio-service/src/main/java/br/com/pessoal/cambioservice/model/Cambio.java
@@ -0,0 +1,24 @@
+package br.com.pessoal.cambioservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cambio implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+}
---
+++ cambio-service/src/test/java/br/com/pessoal/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
