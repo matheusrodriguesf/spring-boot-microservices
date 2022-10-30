4f68fc73c4a305e751838140465e40b8684f93f7
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2022-10-30 11:50:21 -0300

    OPENAPI GATEWAY

---
+++ api-gateway/src/main/java/br/com/pessoal/apigateway/configuration/OpenApiConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.pessoal.apigateway.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    @Lazy(false)
+    public List<GroupedOpenApi> apis(
+            SwaggerUiConfigParameters config,
+            RouteDefinitionLocator locator) {
+        var definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().filter(
+                route -> route.getId().matches(".*-service"))
+                .forEach(route -> {
+                    var name = route.getId();
+                    config.addGroup(name);
+                    GroupedOpenApi.builder()
+                            .group(name)
+                            .pathsToMatch("/" + route.getId() + "/**")
+                            .build();
+
+                });
+        return new ArrayList<>();
+
+    }
+}
